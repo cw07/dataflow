a5ccb5cb1881e7e3fd00addb71bf50f858c1f95a
cw07 <wangchen9617@gmail.com>
2025-10-20 17:01:58 +0800

    schema update

--- src/dataflow/utils/schema_map.py
+++ src/dataflow/utils/schema_map.py
@@ -1,7 +1,8 @@
 from datacore.models.mktdata.realtime import MarketByPrice1
 from datacore.models.mktdata.historical import OHLCV1D
+from datacore.models.mktdata.schema import MktDataSchema
 
 SCHEMA_MAP = {
-    "mbp-1": MarketByPrice1,
-    "ohlcv-1d": OHLCV1D
+    MktDataSchema.MBP_1: MarketByPrice1,
+    MktDataSchema.OHLCV_1D: OHLCV1D
 }
