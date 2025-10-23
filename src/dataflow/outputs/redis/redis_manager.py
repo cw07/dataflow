13bba5d30f0dbad5eb992d5afdb0a07983383ddb
chewang07 <wangchen9617@gmail.com>
2025-10-24 01:43:50 +0800

    bug fixed

--- src/dataflow/outputs/redis/redis_manager.py
+++ src/dataflow/outputs/redis/redis_manager.py
@@ -61,7 +61,9 @@ class RedisManager(BaseOutputManager):
 
     def save(self, message, time_series: TimeSeriesConfig):
         market_data_obj = SCHEMA_MAP[time_series.data_schema].from_dict(message)
-        for output_name in time_series.destination:
-            if output_name in self.redis_instance:
+        for destination_name in time_series.destination:
+            if destination_name in self.redis_instance:
                 logger.info(f"Saving {market_data_obj} for {time_series}")
-                self.redis_instance[output_name].save_data(market_data_obj, time_series)
+                self.redis_instance[destination_name].save_data(market_data_obj, time_series)
+            else:
+                logger.error(f"No active instance for {destination_name}")
