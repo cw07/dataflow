4c77b92a53142a654fe73ce561220f8a55574da4
chewang07 <wangchen9617@gmail.com>
2025-10-28 01:46:29 +0800

    added future options

---
+++ src/dataflow/utils/bbg.py
@@ -0,0 +1,19 @@
+
+
+ROOT_TO_BBG = {
+    'ICE.B':  'CO',  # Crude Oil, Brent
+    'ICE.G':  'QS',  # Gas oil, ICE, Low Su, europe
+    'CME.CL': 'CL',  # Crude Oil, WTI
+    'CME.GC': 'GC',  # Gold
+    'CME.HG': 'HG',  # Copper
+    'CME.PL': 'PL',  # Platinum
+    'CME.SI': 'SI',  # Silver
+    'CME.PA': 'PA',  # Palladium
+    'CME.RB': 'XB',  # RBOB
+    'CME.HH': 'NG',  # Natural Gas
+    'LME.AH': 'LA',  # Aluminum
+    'LME.CA': 'LP',  # Copper
+    'LME.NI': 'LN',  # Nickel
+    'LME.PB': 'LL',  # Lead
+    'LME.ZS': 'LX',  # Zinc
+}
